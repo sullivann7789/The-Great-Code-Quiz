ad54a36c104f425ae6967a955a7443f73e5132c3
Geez-Us <noahsullivan382@gmail.com>
2023-01-18 14:41:54 -0800

    append local storage scores as list first try

--- javalast.js
+++ javalast.js
@@ -3,8 +3,11 @@
  var username = window.localStorage.getItem('username');
  var userscore = "" + username + ":" + displayscore;
  var setuserscore = document.getElementById('wins');
-setuserscore.innerHTML = "High Scores:\n"+ userscore;
+ var addscore = document.createElement('li');
 
+ var addition = addscore.innerHTML = "High Scores:\n"+ "" + userscore;
+ localStorage.setItem('addition', addition);
+ setuserscore.appendChild(addscore);
 
 
 
