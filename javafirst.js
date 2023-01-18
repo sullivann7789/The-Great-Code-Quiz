07906dbbc6c47bcef7f7d28d978f69e6c4ac5a26
Geez-Us <noahsullivan382@gmail.com>
2023-01-18 13:25:16 -0800

    form input/ username debug

--- javafirst.js
+++ javafirst.js
@@ -1,7 +1,9 @@
-var username = document.getElementById('nameinput');
+var username = document.getElementById('nameinput').value;
 var begin = document.getElementById('submit');
-function starttest() {
-    var name = username.innerHTML;
-    localStorage.setItem('username', name);
+function starttest(ev) {
+    ev.preventDefault();
+    localStorage.setItem('username', username);
+    console.log(username);
     window.open('./index1.html');
 };
+begin.addEventListener('click', starttest);
