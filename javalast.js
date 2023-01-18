83b6e709ec6ebdc7545e5fc2b857e188bec2bddb
Geez-Us <noahsullivan382@gmail.com>
2023-01-18 15:49:22 -0800

    styling

--- javalast.js
+++ javalast.js
@@ -1,10 +1,10 @@
 
  var displayscore = window.localStorage.getItem("score");
  var username = window.localStorage.getItem('username');
- var userscore = "" + username + ":" + displayscore;
+ var userscore = "" + username + ": " + displayscore;
  var setuserscore = document.getElementById('wins');
  var addscore = document.createElement('li');
- var addition = addscore.innerHTML = "High Scores:\n"+ "" + userscore;
+ var addition = addscore.innerHTML = "High Scores:\n "+ " " + userscore;
  setuserscore.appendChild(addscore);
  localStorage.setItem('addition', addition);
 
