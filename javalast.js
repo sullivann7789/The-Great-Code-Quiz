322693f6711bd0f9faa03879f7366d3baf5f21ad
Geez-Us <noahsullivan382@gmail.com>
2023-01-18 16:32:28 -0800

    last page button functionality and style

--- javalast.js
+++ javalast.js
@@ -4,10 +4,13 @@
  var userscore = "" + username + ": " + displayscore;
  var setuserscore = document.getElementById('wins');
  var addscore = document.createElement('li');
- var addition = addscore.innerHTML = "High Scores:\n "+ " " + userscore;
+ var addition = addscore.innerHTML = "High Scores:\n "+ "\n " + userscore;
  setuserscore.appendChild(addscore);
  localStorage.setItem('addition', addition);
 
-
+var retest = document.getElementById('reset');
+retest.addEventListener('click', function(){
+    window.open('./index.html');
+});
 
 
