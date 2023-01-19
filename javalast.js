7f162d8493930596f9298ace0327657ebca83791
Geez-Us <noahsullivan382@gmail.com>
2023-01-18 17:01:37 -0800

    margin update

--- javalast.js
+++ javalast.js
@@ -7,7 +7,7 @@
  var addition = addscore.innerHTML = userscore;
  setuserscore.appendChild(addscore);
  localStorage.setItem('addition', addition);
-addscore.setAttribute("style", "margin-left: 8%")
+addscore.setAttribute("style", "margin-left: 10%")
 var retest = document.getElementById('reset');
 retest.addEventListener('click', function(){
     window.open('./index.html');
